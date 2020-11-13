bf9b0a81d6f2567424344e8beb832d5702ed1632
David Mariano Gimenez Mota <davidmariangmota@gmail.com>
2020-11-13 11:34:30 -0300

    Inserção de dados

--- src/controller/final.php
+++ src/controller/final.php
@@ -11,7 +11,7 @@
     <p>Redirecionando para a página da multiplic</p>
     <script>
         setTimeout(function() {
-            window.location.href = "";
+            window.location.href = "https://multiplicnet.com.br/";
         }, 1500);
     </script>
 </body>
